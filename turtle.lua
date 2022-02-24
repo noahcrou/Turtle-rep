4d03d19836b0ef25cc47baade5bd95b169bc28af
noahcrou <53540660+noahcrou@users.noreply.github.com>
2022-02-23 23:30:37 -0500

    Update turtle.lua

--- turtle.lua
+++ turtle.lua
@@ -1,14 +1,9 @@
 isBlock, data = turtle.inspect()
 
-if (isBlock == false) then
-    turtle.place()
-    os.sleep(2)
-end
-
-isBlock, data = turtle.inspect()
-
 if (data['state']['age'] == 7) then
     turtle.dig()
+    turtle.select(1)
+    turtle.place()
 else
     print('is not ready')
 end
